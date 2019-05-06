c4cf305c7dcbe54aa9cc6b90b734eaed99ebae3b
Bishoy Gamal <beshoy.ot@gmail.com>
2019-05-06 03:59:52 +0200

    updated

---
+++ js/z-main.js
@@ -0,0 +1,16 @@
+(function($) {
+  $(document).ready(function() {
+    // $("body").on(
+    //   "DOMSubtreeModified",
+    //   ".grid",
+    //   function() {
+    //     $('.sticky-note i').click(function(){
+    //       console.log($(this).parent().parent().parent())
+    //       $(this).parent().parent().parent().remove();
+    //     });
+    //   });
+  
+  });
+
+
+})(jQuery);
